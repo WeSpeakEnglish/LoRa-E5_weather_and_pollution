a8569daf044ee078040797381811dc002c72a1c7
Aleksei Tertychnyi <cqdx@list.ru>
2023-09-15 22:23:11 +0200

    decoder added

--- decoder.js
+++ decoder.js
@@ -1,22 +1,12 @@
-//// LoRaWan devices automatically base64 encode data sent
-// Sometimes nice just to view what original payload was sent
-//
-// This can help when writing your own decoder
-//
-//
-
  function Decoder(bytes, port, uplink_info) {
   var decoded = {};
-
- decoded.PM2_5 = bytes[0]*100 +  bytes[1];
- decoded.T = bytes[2] +  bytes[3]/100;
- decoded.TH = bytes[4] +  bytes[5]/100;
-            
+ decoded.PM1 = bytes[0]*100 +  bytes[1];
+ decoded.PM2_5 = bytes[2]*100 +  bytes[3];
+ decoded.PM10 = bytes[4]*100 +  bytes[5];
+ decoded.T = bytes[6] +  bytes[7]/100;
+ decoded.TH = bytes[8] +  bytes[9]/100;
+ decoded.RSSI = normalizedPayload.gateways[0].rssi.toFixed(0);
+ decoded.SNR = normalizedPayload.gateways[0].snr.toFixed(0);
+ decoded.FREQUENCY = normalizedPayload.frequency;
   return decoded;
-
  }
-
-// And in the integration section of the Helium console use this JSON
-// For an Adafruit.io integration which is expecting JSON like { "value" : "56" }
-//
-// { "value": {{decoded.payload.value}} }
